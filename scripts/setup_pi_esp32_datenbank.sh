a1e60a4ac1d2f350e0885ebca78e77c563efdbfc
Axel Schmidt <35659663+axelschmidt91@users.noreply.github.com>
2021-04-23 14:56:55 +0200

    restart issue clear, echos refactored

--- scripts/setup_pi_esp32_datenbank.sh
+++ scripts/setup_pi_esp32_datenbank.sh
@@ -1,36 +1,44 @@
 #!/bin/sh
-echo     Update des Pi
-echo *********************
+echo
+echo Update des Pi
+echo 
 sudo apt update && sudo apt upgrade -y
 
-echo     Install Apache
-echo **********************
+echo
+echo Install Apache
+echo 
 sudo apt install apache2 -y
 
 cd /var/www/html
 ls -al
 
-echo     Die IP-Adresse des PI:
-echo ******************************
+echo
+echo Die IP-Adresse des PI:
+echo 
 hostname -I
 
-echo     Install PHP
-echo *******************
+echo
+echo Install PHP
+echo 
 sudo apt install php -y
 sudo rm index.html
 
-echo     index.php erstellen und initieren
-echo *****************************************
+echo
+echo index.php erstellen und initieren
+echo 
 echo "<?php echo "hello world"; ?>" > index.php
 
 sudo service apache2 restart
 
-echo     Install MariaDB und MySQL
-echo *********************************
+echo
+echo Install MariaDB und MySQL
+echo 
 sudo apt install mariadb-server php-mysql -y
 
-echo     Install Security Applications
-echo *************************************
+echo 
+echo Install Security Applications
+echo 
 sudo mysql_secure_installation
 sudo phpenmod mysqli
-sudo service apache restart
+
+sudo service apache2 restart
