e33d8d3cd917b40abfeb451a11109703df56c148
axelschmidt91 <axel.sebastian.schmidt@rwth-aachen.de>
2021-04-23 14:28:20 +0100

    updating setup_pi

--- scripts/setup_pi_esp32_datenbank.sh
+++ scripts/setup_pi_esp32_datenbank.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo
 echo Update des Pi
-echo 
+echo
 sudo apt update && sudo apt upgrade -y
 
 echo
 echo Install Apache
-echo 
+echo
 sudo apt install apache2 -y
 
 cd /var/www/html
@@ -14,32 +14,43 @@ ls -al
 
 echo
 echo Die IP-Adresse des PI:
-echo 
+echo
 hostname -I
 
 echo
 echo Install PHP
-echo 
-sudo apt install phpmyadmin -y
-sudo rm index.html
-
 echo
-echo index.php erstellen und initieren
-echo 
-sudo touch index.php
-sudo echo "<?php echo "hello world"; ?>" > index.php
-
+sudo apt install php -y
+sudo rm index.html
 sudo service apache2 restart
 
 echo
 echo Install MariaDB und MySQL
-echo 
+echo
 sudo apt install mariadb-server php-mysql -y
-
-echo 
-echo Install Security Applications
-echo 
-sudo mysql_secure_installation
-sudo phpenmod mysqli
-
 sudo service apache2 restart
+
+echo
+echo Install Security Applications
+echo
+sudo mysql_secure_installation
+
+echo
+echo Install phpMyAdmin
+echo
+sudo apt install phpmyadmin -y
+sudo phpenmod mysqli
+sudo service apache2 restart
+
+if [!-d /var/www/html/phpmyadmin]; then
+    sudo ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
+fi;
+
+
+echo 
+echo Permission settings
+echo
+ls -lh /var/www/
+sudo chown -R pi:www-data /var/www/html/
+sudo chmod -R 770 /var/www/html/
+ls -lh /var/www/
