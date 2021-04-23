b9d173f877e69e6617e8920fdd03340aa8328a1a
axelschmidt91 <axel.sebastian.schmidt@rwth-aachen.de>
2021-04-23 16:08:55 +0100

    php script und setup_database

---
+++ setup_database.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ $1 -eq 0 ]
+  then
+    echo "No password for user pi provided"
+    break
+fi
+
+echo Create database user pi
+mysql -u root -e "CREATE USER IF NOT EXISTS pi@localhost identified by '$1';"
+echo Grant Privileges
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO pi@localhost;"
+echo Flush Privileges
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+echo Create Database if not exists
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS esp_data CHARACTER SET utf8mb4;"
+mysql -u root -e "USE esp_data; CREATE TABLE IF NOT EXISTS SensorData (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, sensor VARCHAR(30) NOT NULL, location VARCHAR(30) NOT NULL, value1 VARCHAR(10), value2 VARCHAR(10), value3 VARCHAR(10), reading_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)"
