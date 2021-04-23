f40b1075264672d0739bf92e23db31a9a62753d3
Axel Schmidt <35659663+axelschmidt91@users.noreply.github.com>
2021-04-23 11:13:51 +0200

    Update Comments (Echos)

--- scripts/setup_pi_esp32_datenbank.sh
+++ scripts/setup_pi_esp32_datenbank.sh
@@ -1,29 +1,36 @@
-
 #!/bin/sh
-echo Update des Pi
+echo     Update des Pi
+echo *********************
 sudo apt update && sudo apt upgrade -y
 
-echo Install Apache
+echo     Install Apache
+echo **********************
 sudo apt install apache2 -y
 
 cd /var/www/html
 ls -al
 
-echo Die IP-Adresse des PI:
+echo     Die IP-Adresse des PI:
+echo ******************************
 hostname -I
 
-echo Install PHP
+echo     Install PHP
+echo *******************
 sudo apt install php -y
 sudo rm index.html
 
-echo index.php erstellen und 
+echo     index.php erstellen und initieren
+echo *****************************************
 echo "<?php echo "hello world"; ?>" > index.php
 
 sudo service apache2 restart
 
-echo Install MariaDB und MySQL
+echo     Install MariaDB und MySQL
+echo *********************************
 sudo apt install mariadb-server php-mysql -y
-sudo mysql_secure_installation
 
+echo     Install Security Applications
+echo *************************************
+sudo mysql_secure_installation
 sudo phpenmod mysqli
 sudo service apache restart
