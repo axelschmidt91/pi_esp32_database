a37ebbff350a518e7160b04f7eacdbe3094c28f1
axelschmidt91 <axel.sebastian.schmidt@rwth-aachen.de>
2021-04-24 18:43:26 +0100

    script to setup grafana

---
+++ scripts/setup_grafana.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo 
+echo Install Grafana
+echo 
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+
+sudo apt-get update
+sudo apt-get install -y grafana
+
+sudo /bin/systemctl enable grafana-server
+
+sudo /bin/systemctl start grafana-server
+
+echo "Grafana-Server now online at localhost:3000"
